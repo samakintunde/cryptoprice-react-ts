6348936d4f1f1ef1ab14b55bdabfd0fb8ec9ad4e
Samuel Akintunde <samakintunde37@gmail.com>
2019-07-31 14:36:50 +0100

    Updates

---
+++ precache-manifest.db9f9c780d447863909a4e230f03d5a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e294ac5fb917afcb5c12e7f9d340749e",
+    "url": "/cryptoprice-react-ts/index.html"
+  },
+  {
+    "revision": "d48fcb11ce6817d51577",
+    "url": "/cryptoprice-react-ts/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "badf4cc2413ab3d3c5e7",
+    "url": "/cryptoprice-react-ts/static/css/main.095a7be7.chunk.css"
+  },
+  {
+    "revision": "d48fcb11ce6817d51577",
+    "url": "/cryptoprice-react-ts/static/js/2.457d1468.chunk.js"
+  },
+  {
+    "revision": "badf4cc2413ab3d3c5e7",
+    "url": "/cryptoprice-react-ts/static/js/main.598b5a2e.chunk.js"
+  },
+  {
+    "revision": "fa95182e1ab97cf2c35d",
+    "url": "/cryptoprice-react-ts/static/js/runtime~main.d0a2a2ba.js"
+  }
+]);
